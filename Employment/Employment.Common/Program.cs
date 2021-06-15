79f1005ede3051585f1bef2645e5c74aa095f908
Irina Voropaeva <i.vvrpv@gmail.com>
2021-06-15 22:25:06 +0300

    Initial solution setup

---
+++ Employment/Employment.Common/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employment.Common
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Employment/Employment.Services/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employment.Services
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
